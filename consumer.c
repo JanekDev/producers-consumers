12c9c0dbe93965c2cbbc4a9f14f5085e6ed7667d
JanekDev <m.wilinski11@gmail.com>
2022-06-26 22:35:23 +0200

    finished

--- consumer.c
+++ consumer.c
@@ -1,11 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <semaphore.h>
 #include "circular_q.h"
 
 int main() {
